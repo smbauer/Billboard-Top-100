eb823b5496688f34aa2e7bc779ddef92c8e49b9c
smbauer <steve_bauer61@hotmail.com>
2025-10-22 00:25:25 -0400

    Apply .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import os
 import requests
+import spotipy
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
 
+load_dotenv()
+
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+USERNAME = os.getenv("SPOTIFY_USERNAME")
+REDIRECT_URI = "https://example.com"
+
+
+def authenticate_spotify():
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            redirect_uri=REDIRECT_URI,
+            scope="playlist-modify-private",
+            username=USERNAME,
+            show_dialog=True,
+            cache_path="token.txt"
+        )
+    )
+    if sp is None:
+        print("Authentication failed.")
+        return
+    else:
+        return sp
+
+
+# Prompt the user for a date
 date_requested = input("Enter the date you want to use for the Billboard Top 100 list (YYYY-MM-DD): ")
 filename = f"top_100_{date_requested}.html"
 
@@ -30,4 +61,46 @@ artists = [artist.get_text(strip=True) for artist in soup.select("li ul li h3 + 
 # Save the songs and artists to a text file
 with open("songs.txt", "w", encoding="utf-8") as file:
     for i in range(len(songs)):
+    # for song in songs:
         file.write(f"{i + 1}. {songs[i]} by {artists[i]}\n")
+        # file.write(f"{song}\n")
+
+# Authenticate with Spotify
+sp = authenticate_spotify()
+user_id = sp.current_user()["id"]
+
+if sp is None:
+    exit()
+
+song_uris = []
+
+for song in songs:
+    song_query = f"track:{song} year:{date_requested.split('-')[0]}"
+    results = sp.search(q=song_query, type="track")
+
+    if results['tracks']['items']:
+        first_track = results['tracks']['items'][0]
+        song_uris.append(first_track['uri'])
+        print(f"Found track: {first_track['name']} by {first_track['artists'][0]['name']}")
+    else:
+        print(f"No results found for: {song}")
+
+print(f"Total tracks found: {len(song_uris)}")
+
+# Create a new private playlist
+playlist_name = f"Billboard Top 100: {date_requested}"
+playlist_description = f"Top 100 songs from Billboard on {date_requested}"
+
+spotify_playlist = sp.user_playlist_create(
+    user=user_id,
+    name=playlist_name,
+    public=False,
+    description=playlist_description
+    )
+
+playlist_id = spotify_playlist['id']
+
+# Add songs to the newly created playlist
+if song_uris:
+    sp.user_playlist_add_tracks(user=user_id, playlist_id=playlist_id, tracks=song_uris)
+    print(f"Added {len(song_uris)} songs to the playlist '{playlist_name}'.")
